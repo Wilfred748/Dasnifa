a7df60271bf2299eca6c8ce744e3464f6ec8154f
Wilfredo Peralta <113957361+Wilfred748@users.noreply.github.com>
2024-03-12 22:56:32 +0000

    Update Program.cs
    Se organizo el codigo, y ahora el "dotnet run" tira la info necesaria

--- project/Program.cs
+++ project/Program.cs
@@ -1,42 +1,22 @@
-﻿using System;
+using System;
 using SharpPcap;
 using SharpPcap.LibPcap;
 using PacketDotNet;
 
 
-#region Name
-    
-ushort tcpSourcePort = 443;
-ushort tcpDestinationPort = 443;
-var tcpPacket = new TcpPacket(tcpSourcePort, tcpDestinationPort);
 
-var ipSourceAddress = System.Net.IPAddress.Parse("10.0.0.253");
-var ipDestinationAddress = System.Net.IPAddress.Parse("172.217.15.206");
-var ipPacket = new IPv4Packet(ipSourceAddress, ipDestinationAddress);
 
-var sourceHwAddress = "90-90-90-90-90-90";
-var ethernetSourceHwAddress = System.Net.NetworkInformation.PhysicalAddress.Parse(sourceHwAddress);
-var destinationHwAddress = "80-80-80-80-80-80";
-var ethernetDestinationHwAddress = System.Net.NetworkInformation.PhysicalAddress.Parse(destinationHwAddress);
 
-var ethernetPacket = new EthernetPacket(ethernetSourceHwAddress, ethernetDestinationHwAddress, EthernetType.None);
-
-ipPacket.PayloadPacket = tcpPacket;
-ethernetPacket.PayloadPacket = ipPacket;
-
-Console.WriteLine(ethernetPacket.ToString());
-
-byte[] packetBytes = ethernetPacket.Bytes;
-Console.WriteLine(packetBytes);
-#endregion
-
-/*
+#region Captura de servicios y eleccion
 class HIDS
 {
-    #region 
+    
     //para mostrar los servicios que estan corriendo en el momento
     static void Main(string[] args)
     {
+        PackTraffic();
+        Interfaces();
+
         //define una variable que sirve para enlistar los servicios
         CaptureDeviceList devices = CaptureDeviceList.Instance; 
 
@@ -44,6 +24,7 @@ class HIDS
         if (devices.Count < 1)
         {
             Console.WriteLine("No hay servicios");
+            Console.ReadKey();            
             return;
         }
 
@@ -55,9 +36,7 @@ class HIDS
             Console.WriteLine("{0}\n", dev.ToString());
         }
 
-        #endregion
-
-        #region 
+       
         //Elegir un servicio de la lista
         Console.Write("Elige un numero de la lista: ");
         int i = Convert.ToInt32(Console.ReadLine());
@@ -77,12 +56,66 @@ class HIDS
         device.StopCapture();
 
         device.Close();
+        
+    }
 
-        #endregion
+    #region Interfaces MAC
+    static void Interfaces(){
+        // List available network interfaces
+        var devices = CaptureDeviceList.Instance;
+        if (devices.Count == 0)
+        {
+            Console.WriteLine("No se encontraron interfaces de red disponibles.");
+            return;
+        }
+
+        Console.WriteLine("Interfaces de red disponibles:");
+        for (int i = 0; i < devices.Count; i++)
+        {
+            Console.WriteLine($"[{i + 1}] {devices[i].Description} (MAC: {devices[i].MacAddress})");
+        }
+    
+    }
+    #endregion 
+
+    #region Setear info del trafico de paquetes
+    static void PackTraffic(){       
+        //puerto
+    ushort tcpSourcePort = 443;
+    ushort tcpDestinationPort = 443;
+    var tcpPacket = new TcpPacket(tcpSourcePort, tcpDestinationPort);
+        //IP
+    var ipSourceAddress = System.Net.IPAddress.Parse("10.0.0.253");
+    var ipDestinationAddress = System.Net.IPAddress.Parse("172.217.15.206");
+    var ipPacket = new IPv4Packet(ipSourceAddress, ipDestinationAddress);
+        //MAC
+    var sourceHwAddress = "04-7D-7B-67-C3-48";
+    var ethernetSourceHwAddress = System.Net.NetworkInformation.PhysicalAddress.Parse(sourceHwAddress);
+    var destinationHwAddress = "00-00-00-00-00-00";
+    var ethernetDestinationHwAddress = System.Net.NetworkInformation.PhysicalAddress.Parse(destinationHwAddress);
+
+    var ethernetPacket = new EthernetPacket(ethernetSourceHwAddress, ethernetDestinationHwAddress, EthernetType.None);
+
+    ipPacket.PayloadPacket = tcpPacket;
+    ethernetPacket.PayloadPacket = ipPacket;
+
+    Console.WriteLine(ethernetPacket.ToString());
+
+    byte[] packetBytes = ethernetPacket.Bytes;
+    Console.WriteLine(packetBytes);
 
 
     }
+#endregion
+}
+        #endregion
 
+
+
+
+
+#region Algo con muchos fallos 
+/*
     private static void DeviceOnPacketArrival(object sender, CaptureEventArgs Packet e)
     {
         DateTime time = e.Packet.Timeval.Date;
@@ -103,6 +136,5 @@ class HIDS
             time.Millisecond, len);
     }
 
-}
-
-*/
+}*/
+#endregion
